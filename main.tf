5a65e8bcc9b76f847bde77644e94d6c1fe3f58e2
danfrankoenavate <72040696+danfrankoenavate@users.noreply.github.com>
2021-12-02 16:27:51 -0500

    Back to disk

--- main.tf
+++ main.tf
@@ -37,37 +37,6 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = local.vm_name
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_F2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = local.vm_name
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
 
 resource "azurerm_managed_disk" "example" {
   name                 = "${local.vm_name}-disk1"
